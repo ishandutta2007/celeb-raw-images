c2b147d1fdcf826e9f385a124dfd80fc84f47184
ishan dutta <ishandutta2007@gmail.com>
2024-03-20 09:00:57 +0530

    avif

--- to_png.py
+++ to_png.py
@@ -1,9 +1,17 @@
 import os
 from PIL import Image
+import pillow_avif
 
 onlyfiles = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
 for file in onlyfiles:
     if ".webp" in file:
-        print("yeah we need to process", file)
+        print("webp_to_png:yeah we need to process", file)
         im = Image.open(file)
         im.save(file.replace(".webp", ".png"), "PNG")
+
+for file in onlyfiles:
+    if ".avif" in file:
+        print("avif_to_png:yeah we need to process", file)
+        im = Image.open(file)
+        im.save(file.replace(".avif", ".png"), "PNG")
+
