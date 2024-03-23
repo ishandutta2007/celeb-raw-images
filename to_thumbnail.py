2ca53b183ce552bea534ab10355ddc8c0a09dfe2
ishan dutta <ishandutta2007@gmail.com>
2024-03-23 17:02:00 +0530

    billgates_thumbnails/

--- to_thumbnail.py
+++ to_thumbnail.py
@@ -2,7 +2,7 @@ import os
 import math
 from PIL import Image
 
-source_path = os.getcwd() + "/donaldtrump/"
+source_path = os.getcwd() + "/billgates/"
 
 # f = "bbf18ade-1f5f-11eb-99d6-deeedd63f648_image_hires_124435-removebg-preview.png"
 
